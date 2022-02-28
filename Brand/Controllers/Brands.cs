efbb22df3642f8521084a20a8181ccc9768d180c
ilya_tolkunov <ilya4249@gmail.com>
2022-03-01 01:55:54 +0300

    First commit

---
+++ Brand/Controllers/Brands.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+
+
+namespace MyApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Brands : Controller
+    {
+        static List<Entry> entries = new List<Entry>();
+
+        private readonly ILogger<Brands> _logger;
+
+        public Brands(ILogger<Brands> logger)
+        {
+            _logger = logger;
+        }
+        // GET all: api/Brands
+        
+        [HttpGet]
+        [Authorize]
+        public string Read()
+        {
+            string output = "Nothing has been added";
+            if(entries.Count > 0)
+            {
+                output = "";
+                foreach(var ent in entries)
+                {
+                    output += ent.ToString() + "\n";
+                }
+            }
+            return output;
+        }
+
+        // GET api/Brands/{id}
+        
+        [HttpGet("{id}")]
+        [Authorize]
+        public string Read(int id)
+        {
+            string outPut = "Not Found";
+            if((id < entries.Count) && (id >= 0))
+            {
+                outPut = JsonConvert.SerializeObject(entries[id]);
+            }
+            Console.WriteLine(String.Format("Запрошена запись № {0} : {1}", id, outPut));
+            return outPut;
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] Entry ent)
+        {
+            if (ent == null)
+            {
+                return BadRequest();
+            }
+
+            entries.Add(ent);
+            Console.WriteLine(String.Format("Всего записей {0}, послано: {1}", entries.Count, ent.ToString()));
+
+            return new OkResult();
+
+        }
+
+        //Change /api/Brands/{id}
+        [HttpPut("{id}")]
+        public IActionResult Change(int id, [FromBody] Entry ent){
+            if((id<0)||(id>entries.Count)||(ent == null)){
+                return BadRequest();
+            }
+            entries.RemoveAt(id);
+            entries.Insert(id, ent);
+            Console.WriteLine(String.Format("Запись {0} успешно изменена на {1}", id, ent.ToString()));
+            return new OkResult();
+
+        }
+        
+        // DELETE api/Brands/{id}
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            if((id<0) || (id>entries.Count)){
+                return BadRequest();
+            }
+            entries.RemoveAt(id);
+            Console.WriteLine("Запись "+id+" успешно удалена.");
+            return new OkResult();
+        }
+    }
+}
---
+++ Brand/Startup.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens;
+
+namespace Brand
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateAudience = true,
+                    ValidateIssuer = true,
+                    ValidateLifetime = true,
+                    ValidIssuer = "domain.com",
+                    ValidAudience = "domain.com",
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey =
+                        new SymmetricSecurityKey(
+                            System.Text.Encoding.UTF8.GetBytes("Security code for authentification"))
+                };
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseAuthentication();
+            app.UseStaticFiles();
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
+
